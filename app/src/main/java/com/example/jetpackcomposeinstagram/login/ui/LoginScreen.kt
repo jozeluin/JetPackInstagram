75ab2d06d906802da957b25f9df3a99046659fa8
JozeLuin <jozeluin@gmail.com>
2023-12-26 18:51:29 +0100

    Asus.CleanArquitectur

--- app/src/main/java/com/example/jetpackcomposeinstagram/login/ui/LoginScreen.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/login/ui/LoginScreen.kt
@@ -1,4 +1,4 @@
-package com.example.jetpackcomposeinstagram.login
+package com.example.jetpackcomposeinstagram.login.ui
 
 
 import android.app.Activity
@@ -33,7 +33,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
